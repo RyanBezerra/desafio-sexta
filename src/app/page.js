b9310e4713cf8c0036bd681ba8a515ad19ea6cca
kkkkk <ryandonascimentobezerra@gmail.com>
2024-03-08 16:08:57 -0300

    versão 0.3

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,67 @@
 'use client'
+
 import Input from "@/components/input";
 import { cepSchema } from "@/components/zod";
 import { useForm } from 'react-hook-form'
-import { zodRessolver } from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export default function Home() {
+  const { register, handleSubmit, getValues, setValue, formState: {errors} } = useForm({
+    resolver: zodResolver(cepSchema),
+  })
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
         <h1 className="text-3xl text-white font-bold">Formulário de Endereço</h1>
         <form className="flex flex-col py-4 w-1/3 space-y-4">
 
-            <Input id="cep" type="text" label="CEP"/>
-            <Input id="rua" type="text" label="Rua"/>
-            <Input id="numero" type="number" label="Número"/>
-            <Input id="bairro" type="text" label="Bairro"/>
-            <Input id="cidade" type="text" label="Cidade"/>
-            <Input id="estado" type="text" label="Estado"/>
+        <Input
+          id="cep"
+          label="CEP"
+          register={register}
+          error={errors.cep}
+          schema="cep"
+          type="text"
+        />
+        <Input
+          id="rua"
+          label="Rua"
+          register={register}
+          error={errors.rua}
+          schema="rua"
+          type="text"
+        />
+        <Input
+          id="bairro"
+          label="Bairro"
+          register={register}
+          error={errors.bairro}
+          schema="bairro"
+          type="text"
+        />
+        <Input
+          id="cidade"
+          label="Cidade"
+          register={register}
+          error={errors.cidade}
+          schema="cidade"
+          type="text"
+        />
+        <Input
+          id="estado"
+          label="Estado"
+          register={register}
+          error={errors.estado}
+          schema="estado"
+          type="text"
+        />
+        <Input
+          id="numero"
+          label="Número"
+          register={register}
+          error={errors.numero}
+          schema="numero"
+          type="text"
+        />
             
         </form>
     </main>
