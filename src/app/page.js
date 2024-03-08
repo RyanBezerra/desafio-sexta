e7efdaf7f34705b04d064371ac3e799f5280c5b0
kkkkk <ryandonascimentobezerra@gmail.com>
2024-03-08 16:35:02 -0300

    versão 0.6

--- src/app/page.js
+++ src/app/page.js
@@ -67,7 +67,8 @@ export default function Home() {
           schema="numero"
           type="text"
         />
-            
+
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Enviar</button>
         </form>
     </main>
   );
